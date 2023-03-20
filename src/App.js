0d120c8655643052d1ad710dd894f6f5d17ef15a
Moral_Life <leeminam32@gmail.com>
2023-03-20 23:09:30 +0900

    add defaultProps
    #props 대신에 비구조화 할당 이용 가능
    ex) props -> { name, color }
    중괄호 포함
    (함수이름).defaultProps = {
        (attribute이름) : '(값)'
    }

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Hello name ="Minha" color = "red"/>
       <Hello name ="Hani" color = "blue"/>
+      <Hello />
     </div>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from "styled-components";
 
-function Hello(props){
-    console.log(props)
-    return (<div style={{color : props.color}}>
-        안녕하세요 {props.name}님!
+function Hello({name, color}){
+    //console.log(props)
+    return (<div style={{color: color}}>
+        안녕하세요 {name}님!
     </div>);
+}
+Hello.defaultProps = {
+    name: '이름없음',
+    color: "grey"
 }
 
 export default Hello;
