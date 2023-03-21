993bf0fc200ce451608f53df5984d121892fdf2e
Moral_Life <leeminam32@gmail.com>
2023-03-21 23:42:34 +0900

    함수형 렌더링
    기본적인 setNum 방식을 사용하면 비동기적으로 처리가 되기때문에 다음 함수들 명령 안됨, 동기적으로 처리하기 위해 함수형 렌더링 사용
    setNum( preNum => preNum + 1 )
    과 같이 사용

--- src/Counter.js
+++ src/Counter.js
@@ -4,7 +4,10 @@ function Counter(){
     const [num, setNum] = useState(0)
     const onIncrease = () =>{
         //console.log('+1');
-        setNum(num + 1)
+        setNum(preNum => preNum + 1)
+        //함수형 렌더링 : 동기/비동기 테스트
+        //setNum(preNum => preNum + 1)
+        //setNum(num + 1)
     }
     const onDecrease = () =>{
         //console.log('-1');
