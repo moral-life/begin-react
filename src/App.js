b68265670860cad3b236cd516bf1a770a4f178a3
leeminam32 <leeminam32@gmail.com>
2023-04-02 22:58:08 +0900

    배열 항목추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from "./Hello";
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
     const users = [
         {
             id: 1,
@@ -25,6 +38,10 @@ function App() {
     ];
   const nextId = useRef(4);
   const onCreate = () => {
+      setInputs({
+          username: '',
+          email: ''
+      });
       nextId.current += 1;
   };
   return (
@@ -37,6 +54,11 @@ function App() {
       </Wrapper>
       <Counter />
       <InputSample />
+      <CreateUser username={username}
+                  email={email}
+                  onChange={onChange}
+                  onCreate={onCreate}
+      />
       <UserList users={users}/>
     </div>
   );
